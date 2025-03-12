be38dee5638ee127c97dbee53bf7072a06d89146
Beikwaw <minlahdavid@gmail.com>
2025-03-12 13:53:18 +0200

    f1

--- next.config.js
+++ next.config.js
@@ -1,35 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: false,
-        stream: false,
-        util: false,
-        url: false,
-        zlib: false,
-        http: false,
-        https: false,
-        assert: false,
-        os: false,
-        path: false,
-      };
-    }
+  output: 'standalone',
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
     return config;
   },
-  // Disable static exports for pages that require authentication
-  output: 'standalone',
-  experimental: {
-    // This will allow Next.js to tree shake unused exports
-    optimizeCss: true,
-    // This will allow Next.js to optimize the bundle size
-    optimizePackageImports: ['firebase'],
-  },
-}
+};
 
-module.exports = nextConfig 
+export default nextConfig; 
